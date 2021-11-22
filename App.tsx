72fe49c9c6665d46f2bfe0e198473b5dc4a1b772
Phi <34006171@parisnanterre.fr>
2021-11-22 15:26:28 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppContainer from './src/components/app-container' ;
 
 export default function App() {
   return (
+    <AppContainer>
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app! Hello World!</Text>
       <StatusBar style="auto" />
     </View>
+    </AppContainer>
   );
 }
 
