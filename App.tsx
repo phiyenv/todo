417ffa2e07372d8388110ae7c78ee934203ee583
Phi <34006171@parisnanterre.fr>
2021-11-23 19:50:03 +0100

    Add Dark Mode

--- App.tsx
+++ App.tsx
@@ -1,24 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import AppContainer from './src/components/app-container' ;
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import AppContainer from './src/components/app-container' 
+import Main from './src/screens/main'
 
 export default function App() {
   return (
     <AppContainer>
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main />
     </AppContainer>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
