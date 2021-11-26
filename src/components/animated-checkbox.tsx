d16fcadcf1b0f3382add3f6bc4971346281e197f
Phi <34006171@parisnanterre.fr>
2021-11-26 11:15:50 +0100

    fix babel error

--- src/components/animated-checkbox.tsx
+++ src/components/animated-checkbox.tsx
@@ -1,25 +1,68 @@
 import React from 'react'
 import { useEffect, memo } from 'react'
 import Animated, {
-Easing, useSharedValue, withTiming, interpolateColor } from 'react-native-reanimated'
-import Svg, { Path, Defs, ClipPath, G } from 'react-native-svg'
+Easing, useSharedValue, withTiming, interpolateColor, useAnimatedProps } from 'react-native-reanimated'
+import Svg, { Path, Defs, ClipPath, G } from 'react-native-svg';
 
 const MARGIN = 10
 const vWidth = 64 + MARGIN
 const vHeight = 64 + MARGIN
-
 const CheckMarkPath = 
 'M6 27.5L14.1318 44.4708C15.8333 48.0217 20.4361 48.9989 23.4335 46.4455L58 17'
 const outlineBoxPath = 
 'M6 27.5L14.1318 44.4708C15.8333 48.0217 20.4361 48.9989 23.4335 46.4455L58 17'
+const AnimatedPath = Animated.createAnimatedComponent(Path)
 
-const AnimatedCheckbox = () => {
+interface Props {
+    checked?: boolean
+}
+
+
+
+const AnimatedCheckbox = (props: Props) => {
+    const {checked} = props
+    const checkmarkColor = '#000000'
+    const highlightedColor = '#ff000'
+    const boxOutlineColor = '#000000'
+
+    const progress = useSharedValue(0)
+
+    useEffect(() => {
+        progress.value = withTiming(checked ? 1 : 0, {
+            duration : Easing.linear
+        })
+    }, [checked])
+
+    const animatedBoxProps = useAnimatedProps(
+        () => ({
+            stroke: interpolateColor(
+                Easing.bezier(0.16,1,0.3,1)(progress.value),
+            [0,1],
+            ['#00000000', highlightColor],
+            'RGB'
+        ),
+        fill: interpolateColor(
+            Easing.bezier(0.16,1,0.3,1)(progress.value),
+            [0,1],
+            ['#00000000', highlightColor],
+            'RGB'
+        )
+        }), [highlightColor, boxOutlineColor])
+
     return (
         <Svg 
         viewBox={[-MARGIN, -MARGIN, vWidth + MARGIN, vHeight + MARGIN].join(' ')}
         >
-        <Path d={outlineBoxPath} stroke="black" /> 
-        <Path d={CheckMarkPath} stroke="black" />
+         
+    <AnimatedPath 
+        d={outlineBoxPath} 
+        strokeWidth={7} 
+        strokeLinejoin="round" 
+        strokeLinecap="round"
+        animatedProps={animatedBoxProps}
+        />
+    <Path 
+        d={CheckMarkPath} stroke="black" />
 
         </Svg>
     )
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { Text, Box, Center, VStack, 
     themeTools, useTheme, useColorMode, useColorModeValue } from 'native-base'
-
 import ThemeToggle from '../components/theme-toggle'
+import AnimatedCheckbox from '../components/animated-checkbox';
 
 export default function MainScreen(){
     return (
@@ -14,6 +14,11 @@ export default function MainScreen(){
         >
 
         <VStack space={5} alignItems="center">
+            <Box w="100px" h="100px">
+
+                <AnimatedCheckbox></AnimatedCheckbox>
+
+            </Box>    
             <Box bg={useColorModeValue('red.500', 'yellow.500')}>
              <Text>Hello</Text>  
             </Box>
